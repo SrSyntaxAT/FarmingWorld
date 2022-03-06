2d4541af9b2b7b6d56ecef1d3ce833d8cde2f660
SrSyntaxAT <32404041+SrSyntaxAT@users.noreply.github.com>
2022-03-06 17:16:48 +0100

    fix too little distance at the remaining display

--- src/main/java/at/srsyntax/farmingworld/APIImpl.java
+++ src/main/java/at/srsyntax/farmingworld/APIImpl.java
@@ -173,22 +173,22 @@ public final class APIImpl implements API {
     boolean showSeconds = true;
 
     if (days != 0) {
-      stringBuilder.append(days == 1 ? days + " <day>" : days + " <days>");
+      stringBuilder.append(days == 1 ? days + " <day> " : days + " <days> ");
       showSeconds = false;
     }
 
     if (hours != 0) {
-      stringBuilder.append(hours == 1 ? hours + " <hour>" : hours + " <hours>");
+      stringBuilder.append(hours == 1 ? hours + " <hour> " : hours + " <hours> ");
       showSeconds = false;
     }
 
     if (minutes != 0) {
-      stringBuilder.append(minutes == 1 ? minutes + " <minute>" : minutes + " <minutes>");
+      stringBuilder.append(minutes == 1 ? minutes + " <minute> " : minutes + " <minutes> ");
       showSeconds = false;
     }
 
     if (seconds != 0 && showSeconds)
-      stringBuilder.append(seconds == 1 ? seconds + " <second>" : seconds + " <seconds>");
+      stringBuilder.append(seconds == 1 ? seconds + " <second> " : seconds + " <seconds> ");
 
     return stringBuilder.toString();
   }
