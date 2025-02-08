8c2bf79ae0ee2b130dd2d5cf5b31f4aa93c40679
Marcel Haberl <srsyntaxat@gmail.com>
2025-02-08 09:13:00 +0100

    update copyright

--- spigot/src/main/java/at/srsyntax/farmingworld/farmworld/FarmWorldImpl.java
+++ spigot/src/main/java/at/srsyntax/farmingworld/farmworld/FarmWorldImpl.java
@@ -32,7 +32,7 @@ import java.util.concurrent.TimeUnit;
 /*
  * MIT License
  *
- * Copyright (c) 2022-2024 Marcel Haberl
+ * Copyright (c) 2022-2025 Marcel Haberl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
